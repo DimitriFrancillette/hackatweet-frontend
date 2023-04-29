2aa0d7fe3733bba60c19a29efcd3b459fdbe34c2
Dimitri <dfrancillette@gmail.com>
2023-04-29 14:56:59 +0200

    visuel page login fini, début de gestion des modals

--- components/Login.js
+++ components/Login.js
@@ -1,29 +1,56 @@
 import styles from '../styles/Login.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTwitter } from '@fortawesome/free-brands-svg-icons'
+import { Button, Modal } from 'antd';
+import { useState } from 'react';
+
 
 function Login() {
-  return (
-    <div className={styles.main}>
-        <div className={styles.leftSide}>
-        <FontAwesomeIcon className={styles.bigBird} icon={faTwitter} style={{color: "#ffffff",}} />
+
+    const [isSignUpModalOpen, setIsSignUpModalOpen] = useState(false);
+
+    const showModal = () => {
+        setIsSignUpModalOpen(true);
+    };
+
+    const handleOk = () => {
+        setIsSignUpModalOpen(false);
+    };
+
+    const handleCancel = () => {
+        setIsSignUpModalOpen(false);
+    };
+    return (
+        <div className={styles.main}>
+            <div className={styles.leftSide}>
+                <FontAwesomeIcon className={styles.bigBird} icon={faTwitter} style={{ color: "#ffffff", }} />
+            </div>
+            <div className={styles.rightSide}>
+                <div className={styles.bird_div}>
+                    <FontAwesomeIcon icon={faTwitter} className={styles.bird} style={{ color: "#ffffff", }} />
+                </div>
+                <div className={styles.text_div}>
+                    <h1 className={styles.bigTitle}>See what's happening</h1>
+                    <h2 className={styles.smallTitle}>Join Hackatweet today.</h2>
+                </div>
+                <div className={styles.buttons_div}>
+                    <Button type="primary" onClick={() => showModal()} className={styles.signUpbutton}>
+                        Sign up
+                    </Button>
+                    <span className={styles.signText}>Already have an account?</span>
+                    <Button type="primary" onClick={() => showModal()} className={styles.signInbutton}>
+                        Sign in
+                    </Button>
+                </div>
+
+                <Modal title="" open={isSignUpModalOpen} onOk={() => handleOk()} onCancel={() => handleCancel()}>
+                    <p>Some contents...</p>
+                    <p>Some contents...</p>
+                    <p>Some contents...</p>
+                </Modal>
+            </div>
         </div>
-        <div className={styles.rightSide}>
-            <div className={styles.bird_div}>
-            <FontAwesomeIcon icon={faTwitter} className={styles.bird} style={{color: "#ffffff",}} />
-            </div>
-            <div className={styles.text_div}>
-                <h1 className={styles.bigTitle}>See what's happening</h1>
-                <h2 className={styles.smallTitle}>Join Hackatweet today.</h2>
-            </div>
-            <div className={styles.buttons_div}>
-                <button>Sign up</button>
-                <span>Already have an account?</span>
-                <button>Sign in</button>
-            </div>
-        </div>
-    </div>
-  );
+    );
 }
 
 export default Login;
