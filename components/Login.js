346a5ec63fff619bfe0c8c202d664618b0ab4b73
Dimitri <dfrancillette@gmail.com>
2023-04-29 11:25:11 +0200

    avancé sur le visuel de la page login via l'install de fontawesome et en faisant du JSX et CSS

--- components/Login.js
+++ components/Login.js
@@ -1,9 +1,27 @@
 import styles from '../styles/Login.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTwitter } from '@fortawesome/free-brands-svg-icons'
 
 function Login() {
   return (
     <div className={styles.main}>
-
+        <div className={styles.leftSide}>
+        <FontAwesomeIcon className={styles.bigBird} icon={faTwitter} style={{color: "#ffffff",}} />
+        </div>
+        <div className={styles.rightSide}>
+            <div className={styles.bird_div}>
+            <FontAwesomeIcon icon={faTwitter} className={styles.bird} style={{color: "#ffffff",}} />
+            </div>
+            <div className={styles.text_div}>
+                <h1 className={styles.bigTitle}>See what's happening</h1>
+                <h2 className={styles.smallTitle}>Join Hackatweet today.</h2>
+            </div>
+            <div className={styles.buttons_div}>
+                <button>Sign up</button>
+                <span>Already have an account?</span>
+                <button>Sign in</button>
+            </div>
+        </div>
     </div>
   );
 }
